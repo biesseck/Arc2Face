0c1b7df7a69f6f0f5f84b134fd8cae217785072d
Bernardo Biesseck <biesseck@gmail.com>
2026-02-19 10:18:24 -0300

    generate_one_id_by_similarity.py: add parameters '--batch' and '--num-inference-steps'

--- generate_new_ids/generate_one_id_by_similarity.py
+++ generate_new_ids/generate_one_id_by_similarity.py
@@ -28,9 +28,11 @@ def parse_list_arg(arg_string):
 
 def parse_arguments():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--path-input",        type=str, default="/hddevice/nobackup3/bjgbiesseck/datasets/face_recognition/CASIA-WebFace/imgs_crops_112x112/10232/481561.png")   # or /hddevice/nobackup3/bjgbiesseck/datasets/face_recognition/CASIA-WebFace/imgs_crops_112x112_FACE_EMBEDDINGS/10232/10232_mean_embedding_r100_arcface.npy
-    parser.add_argument("--similarity-range",  type=parse_list_arg, default=[0.5,0.69], required=True, help='A list of float values separated by commas, e.g., 0.5,0.69 or [0.5,0.69]')
-    parser.add_argument("--num-samples-by-id", type=int, default=4)
+    parser.add_argument("--path-input",          type=str, default="/hddevice/nobackup3/bjgbiesseck/datasets/face_recognition/CASIA-WebFace/imgs_crops_112x112/10232/481561.png")   # or /hddevice/nobackup3/bjgbiesseck/datasets/face_recognition/CASIA-WebFace/imgs_crops_112x112_FACE_EMBEDDINGS/10232/10232_mean_embedding_r100_arcface.npy
+    parser.add_argument("--similarity-range",    type=parse_list_arg, default=[0.5,0.69], required=True, help='A list of float values separated by commas, e.g., 0.5,0.69 or [0.5,0.69]')
+    parser.add_argument("--num-samples-by-id",   type=int, default=10)
+    parser.add_argument("--batch",               type=int, default=10)
+    parser.add_argument("--num-inference-steps", type=int, default=25)
     args = parser.parse_args()
     return args
 
@@ -153,6 +155,8 @@ def rotate_embedding_by_cosine_similarity(v1: torch.Tensor, cosine_similarity: f
 if __name__ == '__main__':
 
     args = parse_arguments()
+    assert args.num_samples_by_id >= args.batch, f"Error, --num-samples-by-id must be greater or equal to --batch"
+    assert args.num_samples_by_id % args.batch == 0, f"Error, --num-samples-by-id must be a multiple of --batch"
     assert os.path.isfile(args.path_input) or os.path.isdir(args.path_input), f"Error: no such file or dir \'{args.path_input}\'"
 
     pipeline = get_arc2face_model()
@@ -203,15 +207,22 @@ if __name__ == '__main__':
 
     # Generate images:
     print(f'Generating {args.num_samples_by_id} new images...')
-    new_id_emb = project_face_embs(pipeline, new_id_emb)    # pass through the encoder
-    images = pipeline(prompt_embeds=new_id_emb, num_inference_steps=25, guidance_scale=3.0, num_images_per_prompt=args.num_samples_by_id).images
-    
+    new_id_emb_proj = project_face_embs(pipeline, new_id_emb)    # pass through the encoder
+    # images = pipeline(prompt_embeds=new_id_emb_proj, num_inference_steps=25, guidance_scale=3.0, num_images_per_prompt=args.num_samples_by_id).images
+    num_runs = int(args.num_samples_by_id / args.batch)
+    all_generated_images = []
+    for idx_run in range(num_runs):
+            print(f'    run {idx_run}/{num_runs}')
+            images = pipeline(prompt_embeds=new_id_emb_proj, num_inference_steps=args.num_inference_steps, guidance_scale=3.0, num_images_per_prompt=args.batch).images
+            all_generated_images.extend(images)
+
+
     if os.path.isfile(args.path_input):
         output_folder = f"{os.path.splitext(args.path_input)[0]}_newId_sim={similarity}"
     elif os.path.isdir(args.path_input):
         output_folder = f"{os.path.join(args.path_input,args.path_input.split('/')[-1])}_avgEmbedd_newId_sim={similarity}"
     os.makedirs(output_folder, exist_ok=True)
-    for i, img in enumerate(images):
+    for i, img in enumerate(all_generated_images):
         output_img_name = os.path.splitext(os.path.basename(args.path_input))[0]
         path_output_img = os.path.join(output_folder, f"{output_img_name}_newID_newSample_{i}.png")
         print(f"Saving output img: \'{path_output_img}\'")
